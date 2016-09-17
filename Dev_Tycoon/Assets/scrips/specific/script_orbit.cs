9809f1fd64937a0b4b1175fb57f7d851a5fa05ff
AntonShumin <AntonShuminBe@gmail.com>
2016-09-17 22:38:40 +0200

    Fancy camera rotate incl decay test ok

--- Dev_Tycoon/Assets/scrips/specific/script_orbit.cs
+++ Dev_Tycoon/Assets/scrips/specific/script_orbit.cs
@@ -4,19 +4,27 @@ using System.Collections;
 public class script_orbit : MonoBehaviour {
 
     //FIELDS
+
+    //camera rotation target
     public GameObject target = null;
     //auto rotation
     public int rotation_speed = 15;
     public bool auto_orbit = false;
     //drag rotation
     public int drag_speed = 300;
-    public float drag_value = 0;
-    public float drag_decay = 0;
+    private float drag_value_x = 0;
+    private float drag_value_y = 0;
+    public float drag_decay = 0.1F;
     public bool mouse_pressed = false;
+    public float drag_limit = 0.6f;
 
-	
-	// Update is called once per frame
-	void Update () {
+    void Awake()
+    {
+        Application.targetFrameRate = 20;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
         if(target != null)
         {
@@ -40,21 +48,63 @@ public class script_orbit : MonoBehaviour {
 
             if (mouse_pressed)
             {
-                //float rotX = Input.GetAxis("Mouse X") * drag_speed * Mathf.Deg2Rad;
-                float rotation_y = Camera.main.transform.rotation.eulerAngles.y;
-                float rotation_y_adjusted = rotation_y;            
+                //float rotX = Input.GetAxis("Mouse X") * drag_speed * Mathf.Deg2Rad;       
 
-                float input_X = Input.GetAxis("Mouse X") * drag_speed * Time.deltaTime;
-                drag_value += input_X;
-                Debug.Log(drag_value);
-                float input_Y = Input.GetAxis("Mouse Y") * drag_speed * Time.deltaTime / 2;
-                Camera.main.transform.RotateAround(target.transform.position, Vector3.up, input_X);
-                Camera.main.transform.RotateAround(target.transform.position, Camera.main.transform.right, -1*input_Y);
+                float input_X = Input.GetAxis("Mouse X") * Time.deltaTime * 2;
+                if(Mathf.Abs(drag_value_x) < drag_limit) drag_value_x += input_X;
 
 
+                float input_Y = Input.GetAxis("Mouse Y") * Time.deltaTime * -1;
+                if (Mathf.Abs(drag_value_y) < drag_limit)  drag_value_y += input_Y;
+
+
+
+            }
+
+            if(Mathf.Abs(drag_value_x) > 0 || Mathf.Abs(drag_value_y) > 0)
+            {
+                if (Mathf.Abs(drag_value_x) > 0)
+                {
+                    drag_value_x = calculateDecay(drag_value_x);
+                    Camera.main.transform.RotateAround(target.transform.position, Vector3.up, drag_value_x * Time.deltaTime * drag_speed);
+                }
+                if (Mathf.Abs(drag_value_y) > 0)
+                {
+                    drag_value_y = calculateDecay(drag_value_y);
+                    //get camera angle x
+                    float camera_angle_x = Camera.main.transform.rotation.eulerAngles.x;
+                    if (camera_angle_x > 180) camera_angle_x -= 360;
+                    //limit safety net
+                    float angle_upper_limit = 40;
+                    float angle_lower_limit = -10;
+                    float angle_limit_range = (angle_upper_limit - angle_lower_limit);
+                    if (camera_angle_x > angle_upper_limit) camera_angle_x = angle_upper_limit;
+                    if (camera_angle_x < angle_lower_limit) camera_angle_x = angle_lower_limit;
+                    //calculate downwards movement factor
+                    float drag_y_demping = 1;
+                    drag_y_demping = drag_value_y < 0 ? Mathf.Abs( (angle_lower_limit - camera_angle_x)/angle_limit_range) : Mathf.Abs( (angle_upper_limit - camera_angle_x)/angle_limit_range );
+                    //move camera
+                    Camera.main.transform.RotateAround(target.transform.position, Camera.main.transform.right, drag_value_y * Time.deltaTime * drag_speed * drag_y_demping);
+                    //reset drag if at limit
+                    if (drag_y_demping == 0) drag_value_y = 0;  
+                }
             }
         }
 
 	}
+
+    private float calculateDecay(float drag_value)
+    {
+        if (!mouse_pressed)
+        {
+            float drag_frame = drag_value * 2f * Time.deltaTime;
+            drag_value -= drag_frame;
+            if (Mathf.Abs(drag_value) < 0.0005f)
+            {
+                drag_value = 0;
+            }
+        }
+        return drag_value;
+    }
 
 }
