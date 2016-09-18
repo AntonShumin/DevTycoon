d5ae21b6e8b65e72b5b84d1e0c527d63ff881d75
AntonShumin <AntonShuminBe@gmail.com>
2016-09-18 19:14:06 +0200

    message pooling

---
+++ Dev_Tycoon/Assets/scrips/script_messageCenter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class script_messageCenter : MonoBehaviour {
+
+    public GameObject item_prefab;
+    public GameObject grid;
+
+    //Pool
+    public int pooledAmount = 11;
+    List<GameObject> items_list;
+
+
+    void Start()
+    {
+        pupulateList();
+    }
+
+    private void pupulateList()
+    {
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            new_item("", "");
+        }
+    }
+
+    public void create_item()
+    {
+        new_item("HUD ITEM", "H");
+    }
+
+    private void new_item(string para_text, string para_letter)
+    {
+        GameObject item = Instantiate(item_prefab, grid.transform, false) as GameObject;
+        item.transform.SetAsFirstSibling();
+        item.GetComponent<script_messageItem>().set_item(para_text,para_letter);
+        items_list.Add(item);
+        item.SetActive(false);
+    }
+
+    public void set_item(string para_text, string para_letter)
+    {
+
+    }
+}
---
+++ Dev_Tycoon/Assets/scrips/specific/script_messageItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class script_messageItem : MonoBehaviour {
+
+    public GameObject text;
+    public GameObject image;
+    public GameObject letter;
+
+    //item specific vars
+    public int index = 0;
+
+    public void set_item(string para_text, string para_letter)
+    {
+        text.GetComponent<Text>().text = para_text;
+        letter.GetComponent<Text>().text = para_letter;
+    }
+}
