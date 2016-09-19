4616cc188100b37699afb784e4f4a7bb650b9d4a
AntonShumin <AntonShuminBe@gmail.com>
2016-09-19 22:56:06 +0200

    setup singleton

---
+++ Dev_Tycoon/Assets/scrips/script_GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_GameManager : script_singleton<script_GameManager> {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Dev_Tycoon/Assets/scrips/script_gameEvents.cs
+++ Dev_Tycoon/Assets/scrips/script_gameEvents.cs
@@ -5,16 +5,28 @@ public class script_gameEvents : MonoBehaviour {
 
     private string[][] messages;
 
+    void Awake()
+    {
+        setup_messages();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+
+    public void set_message(string index)
+    {
+
+    }
+
+    private void setup_messages()
+    {
+        messages[0][0] = "Develop your first game";
+        messages[0][1] = "G";
+
+    }
 
 
 }
