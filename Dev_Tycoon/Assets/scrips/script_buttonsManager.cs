e33f6e511651136bbb7ca84ee5fb059be37ecd9d
AntonShumin <AntonShuminBe@gmail.com>
2016-09-25 19:04:48 +0200

    button select

---
+++ Dev_Tycoon/Assets/scrips/script_buttonsManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class script_buttonsManager : MonoBehaviour {
+
+    private Button lastPressed;
+    public UnityEngine.EventSystems.EventSystem event_system;
+
+	public void button_pressed(Button button)
+    {
+        if(lastPressed && button.name == lastPressed.name)
+        {
+            event_system.SetSelectedGameObject(null);
+            lastPressed = null;
+        }
+        else
+        {
+            lastPressed = button;
+        }
+        
+
+        if(event_system)
+        {
+            Debug.Log(event_system.name);
+        }
+    }
+
+}
