ee7c8384473ce786d5c3e2c1dd902cc31d4f233a
AntonShumin <AntonShuminBe@gmail.com>
2016-09-25 23:35:00 +0200

    buttons select

--- Dev_Tycoon/Assets/scrips/script_buttonsManager.cs
+++ Dev_Tycoon/Assets/scrips/script_buttonsManager.cs
@@ -5,25 +5,59 @@ using UnityEngine.UI;
 public class script_buttonsManager : MonoBehaviour {
 
     private Button lastPressed;
+
     public UnityEngine.EventSystems.EventSystem event_system;
+    public GameObject[] elements_array = new GameObject[10];
+    public GameObject[] buttons_array = new GameObject[10];
 
 	public void button_pressed(Button button)
     {
-        if(lastPressed && button.name == lastPressed.name)
+
+        int button_index = System.Array.IndexOf(buttons_array, button.gameObject);
+        if(button_index > -1 && button_index < 6)
         {
-            event_system.SetSelectedGameObject(null);
-            lastPressed = null;
+            if (lastPressed && button.name == lastPressed.name)
+            {
+                event_system.SetSelectedGameObject(null);
+                lastPressed = null;
+                button_home();
+                return;
+            } else
+            {
+                lastPressed = button;
+            }
         }
-        else
+        button_event(button.gameObject);
+
+    }
+
+    private void button_event(GameObject button)
+    {
+        if(button == buttons_array[0])
         {
-            lastPressed = button;
+            elements_array[1].SetActive(false);
+            elements_array[2].SetActive(true);
+
+        }else if(button == buttons_array[6])
+        {
+            elements_array[2].SetActive(false);
+            elements_array[3].SetActive(true);
         }
         
+    }
 
-        if(event_system)
+    private void button_home()
+    {
+        foreach(GameObject go in elements_array)
         {
-            Debug.Log(event_system.name);
+            if(go)
+            {
+                go.SetActive(false);
+            }
+            
         }
+        elements_array[0].SetActive(true);
+        elements_array[1].SetActive(true);
     }
 
 }
