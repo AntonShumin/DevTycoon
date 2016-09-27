b49e1c265fd4aec42ea79a37c281571e1475497d
AntonShumin <AntonShuminBe@gmail.com>
2016-09-27 23:06:49 +0200

    still working on the list items

---
+++ Dev_Tycoon/Assets/scrips/specific/scroller/scriipt_scroller_model.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriipt_scroller_model : MonoBehaviour {
+
+    public string name;
+    
+     
+}
---
+++ Dev_Tycoon/Assets/scrips/specific/scroller/script_scroller_view.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using EnhancedUI.EnhancedScroller;
+
+public class script_scroller_view : EnhancedScrollerCellView {
+
+    public Text itemNameText;
+
+    public void SetData(scriipt_scroller_model model)
+    {
+        itemNameText.text = model.name;
+    }
+    	
+}
