68fa11698a8bbcc20664ecd4d8bc8fdc1550f72d
Anton Shumin <AntonShuminBe@gmail.com>
2016-10-02 20:39:51 +0200

    setup calendar

---
+++ Dev_Tycoon/Assets/scrips/script_calendar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_calendar : MonoBehaviour {
+
+    public string[] date_month = new string[12];
+    public int[] date_days = new int[12];
+    
+    private bool time_running = false;
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
--- Dev_Tycoon/Assets/scrips/specific/scroller/scriipt_scroller_model.cs
+++ Dev_Tycoon/Assets/scrips/specific/scroller/scriipt_scroller_model.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class scriipt_scroller_model : MonoBehaviour {
 
-    public string name;
+    //public string name;
     
      
 }
