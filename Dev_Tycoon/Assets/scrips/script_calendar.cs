28326ca725cd596e8bcae774c120594da00d66b7
Anton Shumin <AntonShuminBe@gmail.com>
2016-10-02 22:07:00 +0200

    Date counting test ok

--- Dev_Tycoon/Assets/scrips/script_calendar.cs
+++ Dev_Tycoon/Assets/scrips/script_calendar.cs
@@ -5,22 +5,40 @@ public class script_calendar : MonoBehaviour {
 
     public string[] date_month = new string[12];
     public int[] date_days = new int[12];
+    public float day_tick = 1f;
     
     private bool time_running = false;
+    private int current_month = 0;
+    private int current_day = 1;
+    private int current_year = 1996;
 
-    void Awake()
-    {
-
-    }
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        time_running = true;
+        StartCoroutine("tik_tok");
+    }
+
+    private IEnumerator tik_tok()
+    {
+        string date_string = "";
+        while (true)
+        {
+            yield return new WaitForSeconds(day_tick);
+            if(time_running)
+            {
+                current_day++;
+                if(current_day > date_days[current_month])
+                {
+                    current_day = 1;
+                    current_month++;
+                    if (current_month > 11) current_month = 0;
+                }
+                date_string = "Y"+current_year+" "+date_month[current_month]+" "+current_day;
+                Debug.Log(date_string);
+            }
+                                                   
+        }
+    }
 
 }
