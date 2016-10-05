f35472272a490f71e0d1ee94da33a0b1ce510339
AntonShumin <AntonShuminBe@gmail.com>
2016-10-05 21:37:41 +0200

    calendar count week and progress

--- Dev_Tycoon/Assets/scrips/script_calendar.cs
+++ Dev_Tycoon/Assets/scrips/script_calendar.cs
@@ -5,21 +5,24 @@ public class script_calendar : MonoBehaviour {
 
     public string[] date_month = new string[12];
     public int[] date_days = new int[12];
-    public float day_tick = 1f;
+    public float day_tick = 0.2f;
     
     //calendar vars
     private bool time_running = false;
     private int current_month = 0;
-    private int current_day = 0;
+    private float current_day = 0;
     private int current_year = 1996;
 
     //dev weeks timer
-    private 
+    private float project_day_count;
+    private int project_day_max = 60;
+    private bool project_running = false;
 
 
 	// Use this for initialization
 	void Start () {
         time_running = true;
+        project_running = true;
         StartCoroutine("tik_tok");
     }
 
@@ -27,13 +30,16 @@ public class script_calendar : MonoBehaviour {
     {
         string date_string = "";
         string date_prefix = "";
+        float project_week;
+        float project_progress;
         script_upperFrame o_upperFrame = script_GameManager.Instance.o_ui.upper_frame;
         while (true)
         {
             yield return new WaitForSeconds(day_tick);
             if(time_running)
             {
-                current_day++;
+                //REGULAR DAYS
+                current_day += day_tick;
                 if(current_day > date_days[current_month])
                 {
                     current_day = 1;
@@ -41,10 +47,18 @@ public class script_calendar : MonoBehaviour {
                     if (current_month > 11) current_month = 0;
                 }
                 date_prefix = current_day < 10 ? "0" : "";
-                date_string = "Y"+current_year+" "+date_month[current_month] + " " + date_prefix + current_day;
-                //script_GameManager.Instance.o_ui.upper_frame.set_date(date_string);
+                date_string = "Y"+current_year+" "+date_month[current_month] + " " + date_prefix + Mathf.Floor(current_day);
                 o_upperFrame.set_date(date_string);
-                Debug.Log(date_string);
+               
+                //PROJECT DAYS
+                if(project_running)
+                {
+                    project_day_count += day_tick;
+                    project_week = Mathf.Floor(project_day_count / 7);
+                    project_progress = Mathf.Round((float)project_day_count / (float)project_day_max * 100);
+                    if (project_progress > 100) project_progress = 100; 
+                    o_upperFrame.set_project_date(project_progress,project_week);
+                }
             }
                                                    
         }
--- Dev_Tycoon/Assets/scrips/specific/script_upperFrame.cs
+++ Dev_Tycoon/Assets/scrips/specific/script_upperFrame.cs
@@ -5,15 +5,29 @@ using System.Collections;
 public class script_upperFrame : MonoBehaviour {
 
     public GameObject o_date;
-	public Text text_date;
+	private Text text_date;
+
+    public GameObject o_week;
+    private Text text_week;
+
+    public GameObject o_progress;
+    private Text text_progress;
 
     void Awake()
     {
         text_date = o_date.GetComponent<Text>();
+        text_week = o_week.GetComponent<Text>();
+        text_progress = o_progress.GetComponent<Text>();
     }
 
     public void set_date(string para1)
     {
         text_date.text = para1;
+    }
+
+    public void set_project_date(float project_progress, float project_week)
+    {
+        text_week.text = project_progress + "%";
+        text_progress.text = "W" + project_week;
     }
 }
